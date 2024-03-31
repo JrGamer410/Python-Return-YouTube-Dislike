d81a309c32d8e9a45b079bd61ea70809a156eb98
JrGamer410 <91926668+JrGamer410@users.noreply.github.com>
2024-03-30 23:34:44 -0500

    Fixed the import statement correctly

--- pythonryd.py
+++ pythonryd.py
@@ -4,47 +4,47 @@ import json
 def ryd_getvideoinfo(video_id):
     url = f"https://returnyoutubedislikeapi.com/votes?videoId={video_id}"
     response = requests.get(url)
-    data = json.loads(response)
+    data = json.loads(response.text)
     return data
 
 def ryd_getvideolikes(video_id):
     url = f"https://returnyoutubedislikeapi.com/votes?videoId={video_id}"
     response = requests.get(url)
-    data = json.loads(response)
+    data = json.loads(response.text)
     likes = data['likes']
     return likes
 
 def ryd_getvideodislikes(video_id):
     url = f"https://returnyoutubedislikeapi.com/votes?videoId={video_id}"
     response = requests.get(url)
-    data = json.loads(response)
+    data = json.loads(response.text)
     dislikes = data['dislikes']
     return dislikes
 
 def ryd_getvideorating(video_id):
     url = f"https://returnyoutubedislikeapi.com/votes?videoId={video_id}"
     response = requests.get(url)
-    data = json.loads(response)
+    data = json.loads(response.text)
     rating = data['rating']
     return rating
 
 def ryd_getvideoviews(video_id):
     url = f"https://returnyoutubedislikeapi.com/votes?videoId={video_id}"
     response = requests.get(url)
-    data = json.loads(response)
+    data = json.loads(response.text)
     views = data['viewCount']
     return views
 
 def ryd_isvideodeleted(video_id):
     url = f"https://returnyoutubedislikeapi.com/votes?videoId={video_id}"
     response = requests.get(url)
-    data = json.loads(response)
+    data = json.loads(response.text)
     deleted = data['deleted']
     return deleted
 
 def ryd_getvideodatecreated(video_id):
     url = f"https://returnyoutubedislikeapi.com/votes?videoId={video_id}"
     response = requests.get(url)
-    data = json.loads(response)
+    data = json.loads(response.text)
     dataCreated = data['dateCreated']
     return dataCreated
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.1.3' 
+DESCRIPTION = 'A simple Python library to interact with the Return YouTube Dislike API'
+LONG_DESCRIPTION = 'A simple Python library to interact with the Return YouTube Dislike API'
+
+# Setting up
+setup(
+    name="pythonpyd", 
+    version=VERSION,
+    author="Sean-e",
+    author_email="<seane410@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    py_modules=['pythonryd'],  # Include pythonryd.py directly as a module
+    install_requires=[],  # Add any additional packages that need to be installed along with your package. Eg: 'requests'
+    keywords=['python', 'api'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
